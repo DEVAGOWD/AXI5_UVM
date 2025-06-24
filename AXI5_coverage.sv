d26c29b56c52334b9d4a07fdc2614d661c25c52f
DEVAGOWD <devendragowdmorla123@gmail.com>
2025-06-24 10:18:09 +0530

    AXI5_coverage.sv

---
+++ AXI5_coverage.sv
@@ -0,0 +1,15 @@
+class AXI5_coverage extends uvm_component;
+
+//=====================factory registration ============================
+
+	`uvm_component_utils(AXI5_coverage)
+
+//============================construction========================
+
+	function new(string name="",uvm_component parent);
+		super.new(name,parent);
+	endfunction
+
+
+
+endclass
